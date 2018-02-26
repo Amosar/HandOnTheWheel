df6489af4aefa9565db07787fd8d56f4e1c23c89
Amosar <maxi.cass11@gmail.com>
2018-02-26 11:22:49 +0000

    init commit for test

--- googleMapHook/script.js
+++ googleMapHook/script.js
@@ -1,67 +1,81 @@
-window.onload=function(){
-	var id, target, options;
+var id;
+var locations = [];
 
-	function success(pos) {
-		var crd = pos.coords;
-	  initMap(crd)
-          navigator.geolocation.clearWatch(id);
-	}
 
-	function error(err) {
-	  var crd = {
-		latitude: 51.507222,
-		longitude: -0.1275
-	  };
-	  initMap(crd)
-	  navigator.geolocation.clearWatch(id);
-	}
+var defaultLocation = {
+    lat: 51.507222,
+    lng: -0.1275
+};
 
-	options = {
-	  enableHighAccuracy: false,
-	  timeout: 5000,
-	  maximumAge: 0
-	};
-
-	function initMap(coords) {
-		var lat = coords.latitude;
-	  var lng = coords.longitude;
-	  var location = {
-		lat: lat,
-		lng: lng
-	  };
-	  map = new google.maps.Map(document.getElementById('map'), {
-		center: location,
-		zoom: 15
-	  });
-	  infowindow = new google.maps.InfoWindow();
-	  var service = new google.maps.places.PlacesService(map);
-	  service.nearbySearch({
-		  location: location,
-		  radius: 5000,
-		  keyword: ['bar']
-		},
-		callback);
-	}
-
-	function callback(results, status) {
-	  if (status === google.maps.places.PlacesServiceStatus.OK) {
-		for (var i = 0; i < results.length; i++) {
-		  createMarker(results[i]);
-		}
-	  }
-	}
-
-	function createMarker(place) {
-	  var placeLoc = place.geometry.location;
-	  var marker = new google.maps.Marker({
-		map: map,
-		position: place.geometry.location
-	  });
-	  google.maps.event.addListener(marker, 'click', function() {
-		infowindow.setContent(place.name);
-		infowindow.open(map, this);
-	  });
-	 }
-
-	id = navigator.geolocation.watchPosition(success, error, options);
+function initialize() {
+    var options = {
+        enableHighAccuracy: false,
+        timeout: 5000,
+        maximumAge: 0
+    };
+    id = navigator.geolocation.watchPosition(success, error, options);
 }
+
+function success(pos) {
+    var location = {
+        lat: pos.coords.latitude,
+        lng: pos.coords.longitude
+    };
+    initMap(location);
+    navigator.geolocation.clearWatch(id);
+}
+
+function error() {
+    var location = {
+        lat: defaultLocation.lat,
+        lng: defaultLocation.lng
+    };
+    initMap(location);
+    navigator.geolocation.clearWatch(id);
+}
+
+
+function initMap(location) {
+    var mapOptions = {
+        center: location,
+        zoom: 15
+    };
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    placeMarkers(location)
+}
+
+function placeMarkers(location) {
+    var request = {
+        location: location,
+        radius: '5000',
+        types: ['bar']
+    };
+    var service = new google.maps.places.PlacesService(map);
+
+    //With cluster
+    service.nearbySearch(request, function (results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+                var place = results[i];
+                var markerLocation = {
+                    lat: place.geometry.location.lat(),
+                    lng: place.geometry.location.lng()
+                };
+                locations.push(markerLocation);
+                var labels = '';
+
+                var markers = locations.map(function (location, i) {
+                    return new google.maps.Marker({
+                        position: location,
+                        label: labels[i % labels.length]
+                    });
+                });
+
+                var markerCluster = new MarkerClusterer(map, markers,
+                    {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+            }
+        }
+    });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
